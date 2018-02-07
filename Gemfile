7b9b3569e0ca35dcab382e3f3bd1849e8ee783e8
Royal Summers <rcrc001@gmail.com>
2018-02-07 15:57:45 -0700

    added comments

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ end
 
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'acts_as_votable', '~> 0.11.1'
+gem 'simple_form', '~> 3.5'
+gem 'record_tag_helper', '~> 1.0'
 
 gem 'rails', '~> 5.1.4'
 gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :submissions do
     member do
       put "Like", to: "submission#upvote"
       put "Dislike", to: "submission#downvote"
+    end
+    resources :comments 
+  end
   root to: "submissions#index"
+
  
 end
